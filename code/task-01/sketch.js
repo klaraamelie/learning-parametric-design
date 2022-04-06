e51b57e915bd70c07b4d6c1e6f2895a0418914b7
klaraamelie <103108746+klaraamelie@users.noreply.github.com>
2022-04-06 15:23:03 +0200

    task-01 creation

--- code/task-01/sketch.js
+++ code/task-01/sketch.js
@@ -4,9 +4,44 @@ function preload(){
 }
 
 function setup() {
-  //createCanvas(400, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(100);
+  background(0,0,51);
+  fill('rgba(0,0,0,0)');
+  stroke(255);
+  strokeWeight(1)
+  circle(200, 200, 200 );
+  circle(50,285,50);
+  circle(100, 250, 90 );
+  arc(
+    200, 200, 
+    280, 280, 
+    Math.PI / 180 * -70,
+    Math.PI / 180 * 100,
+    OPEN);
+
+    arc(
+      199, 199, 
+      220, 220, 
+      Math.PI / 180 * -90,
+      Math.PI / 180 * 60,
+      OPEN);
+    
+      arc(
+        200, 200, 
+        240, 240, 
+        Math.PI / 180 * -80,
+        Math.PI / 180 * 85,
+        OPEN);
+  line(200,250,280,20);  
+  strokeWeight(2,5);
+  point(20, 20);
+  point(380, 380);
+
+  
+  
+        
+  
 }
